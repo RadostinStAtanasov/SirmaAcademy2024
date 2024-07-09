f5e86111c9958e4979c85d5e5bfe2f833a86bf06
RadostinAtanasov <radostin.st.atanasov@gmail.com>
2024-07-09 18:09:09 +0300

    DOM Exercises

---
+++ DOM/script1.js
@@ -0,0 +1,7 @@
+function restyle() {
+    const getElement = document.getElementById('text');
+
+    getElement.style.color = 'red';
+    getElement.style.fontFamily = 'arial';
+    getElement.style.fontSize = '24px';
+}
