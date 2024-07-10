1723ce4b76d531399596045d55ca3cc84a64eb38
RadostinAtanasov <radostin.st.atanasov@gmail.com>
2024-07-10 12:30:35 +0300

    third exercise

---
+++ DOM/script3.js
@@ -0,0 +1,6 @@
+function changeParagraphColor() {
+    const paragraph = document.getElementById('changeColor');
+
+    paragraph.style.background = 'red';
+    paragraph.style.background = 'blue';
+}
